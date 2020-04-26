1d3d5c0d4fb1cd363cbc9969c83365512442c38a
shiningartemis <shiningartemis@outlook.com>
2020-04-26 11:42:53 +0800

    1.2.2版：     加入shiro，写入基础拦截逻辑，还未写认证逻辑

---
+++ src/main/java/cn/sora/shiro/AccountUserRealm.java
@@ -0,0 +1,33 @@
+package cn.sora.shiro;
+
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * 自定义Realm
+ * @author sora
+ *
+ */
+public class AccountUserRealm extends AuthorizingRealm {
+
+    /**
+     * 执行授权逻辑(权限)
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        System.out.println("执行授权逻辑");
+        return null;
+    }
+
+    /**
+     * 执行认证逻辑(登陆)
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        System.out.println("执行认证逻辑");
+        return null;
+    }
+}
